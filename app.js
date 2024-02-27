656dfa8210662009a261b7e3dc1df1a39a0c792f
Zachary Kohs <zachary.c.kohs@gmail.com>
2024-02-27 09:49:10 -0500

    created a .env file to hold the api keys and passed them to the view in the app.js file

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
 // Listen on port 3000
 app.listen(port, () => console.info(`App listening on port ${port}`));
 
@@ -11,3 +13,8 @@ app.use(express.static("public"));
 // Set View's
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
+// Navigation
+app.get('', (req, res) => {
+    res.render('index', { city_api_key: process.env.CITIES_API_KEY, weather_api_key: process.env.WEATHER_API_KEY })
+})
